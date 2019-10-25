e55500fd5a0b7da499c190918fc3418d7acf7796
Jasterix <ljcalixte@gmail.com>
2019-10-25 01:45:33 -0400

    refactor/created board model

---
+++ server/models/board.js
@@ -0,0 +1,64 @@
+
+  create_table "boards", force: :cascade do |t|
+  t.string "name"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+  t.string "urls", default: "{}"
+  t.string "codes", default: "{}"
+  t.integer "likes", default: 0
+end
+
+
+export default (sequelize, DataTypes) => {
+    const Board = sequelize.define('Board', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: 'Please enter the title for your board'
+          },
+          unique: {
+            args: true,
+            msg: 'Oops, looks like that board name is taken!'
+        }
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: 'Please enter an author'
+        }
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'User',
+          key: 'id',
+          as: 'userId',
+        }
+      }
+    }, {});
+    Board.associate = (models) => {
+      // associations can be defined here
+      Board.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
+    };
+    return Board;
+  };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
-import Users from '../controllers/user';
-import Books from '../controllers/book';
+// import Users from '../controllers/user';
+// import Books from '../controllers/book';
+
+import Boards from '../controllers/boards'
 
 export default (app) => {
 
 app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the bookStore API!',
+    message: 'Welcome to the Status API!',
 }));
 
 app.post('/api/users', Users.signUp); // API route for user to signup
@@ -13,4 +15,26 @@ app.get('/api/books', Books.list); // API route for user to get all books in the
 app.put('/api/books/:bookId', Books.modify); // API route for user to edit a book
 app.delete('/api/books/:bookId', Books.delete); // API route for user to delete a book
 
+const notes = require('../controllers/note.controller.js');
+
+// Create a new Note
+app.post('/notes', notes.create);
+
+// Retrieve all Notes
+app.get('/notes', notes.findAll);
+
+// Retrieve a single Note with noteId
+app.get('/notes/:noteId', notes.findOne);
+
+// Update a Note with noteId
+app.put('/notes/:noteId', notes.update);
+
+// Delete a Note with noteId
+app.delete('/notes/:noteId', notes.delete);
+}
+    
+get 'users/index'
+resources :users
+resources :boards
+resources :tiles
 };
